a89334746c9166dc468b89b98f0b25c447277865
userri <dladbfl67@naver.com>
2026-05-13 17:40:14 +0900

    feat: 제출 성공시 에러는 초기화

--- app/src/main/java/com/example/pinq_frontend/ui/quiz/QuizSessionViewModel.kt
+++ app/src/main/java/com/example/pinq_frontend/ui/quiz/QuizSessionViewModel.kt
@@ -75,6 +75,7 @@ class QuizSessionViewModel(
                             lastAnswer = result,
                             correctCount = if (result.isCorrect) it.correctCount + 1
                             else it.correctCount,
+                            error = null, // 제출 성공했으니 에러는 초기화
                         )
                     }
                 }
