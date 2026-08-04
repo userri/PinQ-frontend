9f45ce64b393014bae78c2ad2820065705c84dca
imyuri <dladbfl67@naver.com>
2026-08-05 01:03:46 +0900

    chore: 정식 출시2 (14)로 버전 업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,8 @@ android {
         applicationId = "com.finq.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.2.1"
+        versionCode = 14
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
